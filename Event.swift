75b5df794edb0f86963efca40a9c2945a175675b
Marikossa <98408343+Marikossa@users.noreply.github.com>
2026-02-28 00:45:19 +0400

    Add Event model for iOS calendar app

--- Event.swift
+++ Event.swift
@@ -1,14 +1,50 @@
 import Foundation
 
-struct Event {
+struct Event: Identifiable, Codable {
+    var id: String?
     var title: String
-    var date: Date
+    var description: String
+    var startDate: Date
+    var endDate: Date?
+    var allDay: Bool = false
+    var eventType: String
+    var color: String
+    var icon: String
     var recurrence: Recurrence?
+    var reminders: [Reminder] = []
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case title
+        case description
+        case startDate = "start_date"
+        case endDate = "end_date"
+        case allDay = "all_day"
+        case eventType = "event_type"
+        case color
+        case icon
+        case recurrence
+        case reminders
+    }
 }
 
-enum Recurrence {
-    case daily
-    case weekly
-    case monthly
-    case yearly
+struct Recurrence: Codable {
+    var type: String
+    var interval: Int
+    var endDate: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case type
+        case interval
+        case endDate = "end_date"
+    }
+}
+
+struct Reminder: Codable, Identifiable {
+    var id = UUID()
+    var minutesBefore: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case minutesBefore = "minutes_before"
+    }
 }
